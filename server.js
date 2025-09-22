6051dec57756f2403760d8f48b91cfa7cef715bc
bbjaat5310-eng <bbjaat5310@gmail.com>
2025-09-22 23:35:22 +0530

    vercel

--- server.js
+++ server.js
@@ -28,11 +28,12 @@ if (!fs.existsSync(uploadDir)) {
 
 // Middleware
 app.use(express.json());
-app.use(
-  cors({
-    origin: "*",
-  })
-);
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
 // Use the routes from the dedicated router file, passing the data store
 app.use('/', createLeadRoutes(inMemoryStore, upload));
